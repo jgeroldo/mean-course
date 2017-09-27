f95399bfcbfbc931abdad5bb08bd6dbbf4429480
João Paulo Geroldo <joao.geroldo@2gather.com.br>
2017-09-27 00:15:57 -0300

    Express configure! Module 1

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,44 @@
 var hotelsData = require('../data/hotel-data.json');
 
+
 module.exports.hotelsGetAll = function(req, res) {
   console.log("GET the hotels");
+  console.log(req.query);
+
+  var offset = 0;
+  var count = 5;
+
+  if (req.query && req.query.offset) {
+    offset = parseInt(req.query.offset, 10);
+  }
+
+  if (req.query && req.query.count) {
+    count = parseInt(req.query.count, 10);
+  }
+
+  var returnData = hotelsData.slice(offset, offset + count);
+
   res
     .status("200")
-    .send(hotelsData);
+    .send(returnData);
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+  /* getting the url paramter */
+  var hotelId = req.params.hotelId;
+  /* getting the hotel data */
+  var thishotel = hotelsData[hotelId];
+  console.log("GET the hotelId: ", hotelId);
+  res
+    .status("200")
+    /* show the hotel data */
+    .send(thishotel);
+};
+
+module.exports.hotelsAddOne = function(req, res) {
+  console.log("Post a New hotel");
+  console.log(req.body);
+  res
+    .status("200")
+    .json(req.body);
 };
--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require('express');
 var path = require('path');
 /* using the Express */
 var app = express();
-
+/* parse parameter from query parser */
+var bodyParser = require('body-parser');
 /* adding a routes folder inside the app */
 var routes = require('./api/routes');
 
@@ -18,6 +19,9 @@ app.use(function (req, res, next) {
 
 /* defining a default directory for static files */
 app.use(express.static(path.join(__dirname,'public')));
+
+/* adding the middleware bodyParser */
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* indicating to express where are the routes */
 app.use('/api', routes);
